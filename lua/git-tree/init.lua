b8dad0de380df13dba4a017fc04d55f4121da488
Denys Kotelovych <denys.kotelovych@gmail.com>
2021-08-31 01:03:17 +0300

    Git history small func;

--- lua/git-tree/init.lua
+++ lua/git-tree/init.lua
@@ -1,1 +1,9 @@
-print("May the force be with you")
+local function git_hist()
+	vim.cmd(":split | terminal")
+	local command = ':call jobsend(b:terminal_job_id, "gh -30\\n")'
+	vim.cmd(command)
+end
+
+return {
+	git_hist = git_hist,
+}
